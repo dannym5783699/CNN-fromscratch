ddf132d71abec8acc1335d0e9fe4268fce80c1e3
Nicholas Livingstone <nicholaselmolivingstone@gmail.com>
2024-10-27 03:11:13 -0600

    Improved existing code for batch training

--- midterm_nueralnetworks/neural_network/layer.py
+++ midterm_nueralnetworks/neural_network/layer.py
@@ -1,4 +1,5 @@
 import numpy as np
+from midterm_nueralnetworks.neural_network.activation import activation_funcs, activation_derivatives
 
 class Layer:
     """
@@ -6,7 +7,7 @@ class Layer:
     with the bias term absorbed into the weight matrix.
     """
 
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size : int, output_size : int, activation : str = "relu"):
         """
         Initializes the Layer class.
 
@@ -16,13 +17,25 @@ class Layer:
             The number of neurons in the previous layer (or input layer).
         output_size : int
             The number of neurons in the current layer (or output layer).
+        activation : str
+            The activation function to use for the layer. Default is 'relu'.
         """
-        self.activation = lambda x:x
-        self.actDer = lambda _:1
+
+        try:
+            self.activation = activation
+            self._activation_func = activation_funcs[activation]
+            self._activation_derivative = activation_derivatives[activation]
+        except KeyError:
+            raise ValueError(f"Activation function {activation} not supported")
+
+        self.prev_input = None
+        self.activations = None
+        self.preactivations = None
+        self.grad_weights = None
 
         self.weights = np.random.randn(output_size, input_size + 1)
 
-    def forward(self, inputs):
+    def forward(self, X):
         """
         Performs a forward pass through the layer by calculating the weighted sum of inputs and the bias.
 
@@ -43,22 +56,42 @@ class Layer:
         already contains the bias information.
         """
 
-        inputs_with_bias = np.append(inputs, 1)
+        self.prev_input = X
+        self.preactivations = np.dot(self.concat_bias(X), self.weights.T)
+        self.activations = self._activation_func(self.preactivations)
 
-        self.z = np.dot(self.weights, inputs_with_bias)
-        return self.z
-    
+        return self.activations
 
-    def setActivation(self, activation, derivative):
-        self.activation = activation
-        self.actDer = derivative
-        return self
-    
-    def applyActivation(self, x):
-        return self.activation(x)
-    
-    def activation_derivative(self, x):
-        return self.actDer(x)
+    def backward(self, delta):
+        """
+        Performs the backward pass, calculating gradients for the layer's weights and the delta to pass to previous layers.
 
-    
+        Parameters:
+        ----------
+        delta : numpy.ndarray
+            The error signal from the subsequent layer, scaled by the derivative of the loss with respect to
+            this layer's output.
 
+        Returns:
+        -------
+        numpy.ndarray
+            The delta to propagate to the previous layer.
+        """
+        # Calculate the derivative of the activation function on the preactivations
+
+        delta *= self._activation_derivative(self.preactivations)
+
+        prev_input_with_bias = self.concat_bias(self.prev_input)
+
+        # Compute the gradient for weights as the outer product of delta and previous input
+        self.grad_weights = np.dot(delta.T, prev_input_with_bias) / delta.shape[0]
+
+        # Compute the delta to pass to the previous layer
+        delta_prev = np.dot(delta, self.weights[:, :-1])
+
+        return delta_prev
+    
+    @staticmethod
+    def concat_bias(X : np.ndarray) -> np.ndarray:
+        """Concatenates a bias term to the input data."""
+        return np.concatenate([X, np.ones((X.shape[0], 1))], axis=1)
--- midterm_nueralnetworks/neural_network/loss.py
+++ midterm_nueralnetworks/neural_network/loss.py
@@ -10,3 +10,8 @@ def cross_entropy_derivative(output, target):
     Derivative of Cross-Entropy loss with respect to the output.
     """
     return - (target / output) + ((1 - target) / (1 - output))
+
+get_loss_derivative = {
+    "mse": mse_derivative,
+    "cross_entropy": cross_entropy_derivative
+}

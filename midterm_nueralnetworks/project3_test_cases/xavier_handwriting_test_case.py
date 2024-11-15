12e7a94d0c8dc6cae4577282c075255ecbe098e2
Key Vollers <keyvollers@unm.edu>
2024-11-15 13:00:26 -0700

    Adding in proper filing

--- midterm_nueralnetworks/project3_test_cases/xavier_handwriting_test_case.py
+++ midterm_nueralnetworks/project3_test_cases/xavier_handwriting_test_case.py
@@ -9,7 +9,7 @@ from midterm_nueralnetworks.neural_network.layer import Layer
 from midterm_nueralnetworks.neural_network.loss import NLL_derivative_softmax
 
 
-def main():
+def batch_size_test_he_weight():
     print("Preparing data")
     X, Y = load_digits(return_X_y=True)
 
@@ -68,6 +68,65 @@ def main():
     plt.tight_layout()
     plt.show()
 
+def batch_size_test_xavier_weight():
+    print("Preparing data")
+    X, Y = load_digits(return_X_y=True)
+
+    encoder = OneHotEncoder()
+    Y = encoder.fit_transform(Y.reshape(-1, 1)).toarray()
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.05, random_state=42, stratify=Y)
+
+    MAX_EPOCHS = 500
+    LR = 1e-3
+    batch_sizes = [8, 32, 64, 128]  # List of batch sizes to experiment with
+
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10), sharey=True)
+
+    for batch_size, ax in zip(batch_sizes, axs.flatten()):
+
+        # Initialize the network
+        net = FeedforwardNeuralNetwork([
+            Layer(64, 64, "relu", weight_init="xavier_uniform"),
+            Layer(64, 32, "relu", weight_init="xavier_uniform"),
+            Layer(32, 10, "softmax", final_layer=True, weight_init="xavier_uniform")
+        ])
+
+        train_losses = np.zeros(MAX_EPOCHS)
+        test_losses = np.zeros(MAX_EPOCHS)
+
+        print(f"Training model with batch size {batch_size}")
+        for epoch in range(MAX_EPOCHS):
+            batch_losses = []
+            for i, (x_batch, y_batch) in enumerate(get_batches(X_train, Y_train, batch_size)):
+                y_pred = net.forward(x_batch)
+                batch_losses.append(nll_loss(y_pred, y_batch))
+                net.backward(y_pred, y_batch, NLL_derivative_softmax)
+                net.gd(LR)
+                net.zero_grad()
+            mean_train_loss = np.mean(batch_losses)
+            test_loss = nll_loss(net.forward(X_test), Y_test)
+            net.zero_grad()
+
+            train_losses[epoch] = mean_train_loss
+            test_losses[epoch] = test_loss
+
+            if epoch % 10 == 0:
+                print(f"Epoch {str(epoch).zfill(2)}, Mean Train Loss: {mean_train_loss}, Test Loss: {test_loss}")
+
+        ax.plot(train_losses, label="Train Loss")
+        ax.plot(test_losses, label="Test Loss")
+        ax.set(
+            xlabel="Epoch",
+            ylabel="Log Loss",
+            title=f"Batch Size: {batch_size}"
+        )
+        ax.set_yscale('log')
+        ax.legend()
+
+    plt.tight_layout()
+    plt.show()
+
 
 def get_batches(X, Y, batch_size):
     indices = np.arange(len(X))
@@ -85,4 +144,5 @@ def nll_loss(output, target):
 
 
 if __name__ == "__main__":
-    main()
+    batch_size_test_he_weight()
+    batch_size_test_xavier_weight()
